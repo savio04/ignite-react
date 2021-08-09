bde939aad994e8add827e397f1296d1eb902810c
savio04 <savio27.05.2001@gmail.com>
2021-08-09 17:50:32 -0300

    add button types

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer, TypeButton } from "./styles";
 import Modal from "react-modal";
+import incomeImg from "../../assets/Entradas.svg";
+import outcomeImg from "../../assets/Saídas.svg";
+import closeImage from "../../assets/Vector.svg";
+import { useState } from "react";
 
 interface INewTransactionModal {
   isOpen: boolean;
@@ -10,6 +14,7 @@ export function NewTransactionModal({
   isOpen,
   onCloseModal,
 }: INewTransactionModal) {
+  const [type, setType] = useState("deposit");
   return (
     <Modal
       isOpen={isOpen}
@@ -17,10 +22,38 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onCloseModal}
+        className="react-modal-close"
+      >
+        <img src={closeImage} alt="Fechar modal" />
+      </button>
       <Container>
+        <h2>Cadastro de transação</h2>
         <input placeholder="Titulo" />
 
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <TypeButton
+            type="button"
+            onClick={() => setType("deposit")}
+            isActive={type === "deposit"}
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </TypeButton>
+
+          <TypeButton
+            type="button"
+            onClick={() => setType("withdraw")}
+            isActive={type === "withdraw"}
+          >
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saida</span>
+          </TypeButton>
+        </TransactionTypeContainer>
 
         <input placeholder="Categoria" />
 
--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,88 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { darken } from "polished";
+
+interface TypeButtonProps{
+    isActive:boolean
+}
 
 export const Container = styled.form`
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  //style de input de exemplo
+  input {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    border-radius: 0.25rem;
+    border: solid 1px #d7d7d7;
+    background: #e7e9ee;
+
+    font-weight: 400;
+    font-size: 1rem;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button[type="submit"] {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    background: var(--green);
+    color: #fff;
+    border-radius: 0.25rem;
+    border: 0;
+    margin-top: 1.5rem;
+    transition: filter 0.2s;
+    font-weight: 600;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.5rem;
+`;
+
+export const TypeButton = styled.button<TypeButtonProps>`
+  height: 4rem;
+  border: solid 1px #d7d7d7;
+  border-radius: 0.25rem;
+
+  background: ${(props) => props.isActive ? '#EEEE': 'transparent'};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
 `;
