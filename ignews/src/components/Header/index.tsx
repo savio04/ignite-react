010f0f2d7c3573032f5b306aca5f2397171e623e
savio04 <savio27.05.2001@gmail.com>
2021-08-12 10:26:15 -0300

    add ignews

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import styles from './styles.module.scss'
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Image 
+                    src="/images/logo.svg"
+                    alt="ignews"
+                    width={108}
+                    height={30}
+                />
+                <nav>
+                    <a className={styles.active} >Home</a>
+                    <a>Posts</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
